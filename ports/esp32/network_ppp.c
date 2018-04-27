092f8b71bf002cd05e26a6652fb3db03585f6254
Eric Poulsen <eric@zyxod.com>
2018-04-27 15:13:06 -0700

    updates for ppp

--- ports/esp32/network_ppp.c
+++ ports/esp32/network_ppp.c
@@ -25,90 +25,255 @@
  * THE SOFTWARE.
  */
 
+
 #include "py/runtime.h"
 #include "py/mphal.h"
+#include "py/objtype.h"
+#include "netutils.h"
 
-#include "eth_phy/phy.h"
-#include "eth_phy/phy_tlk110.h"
-#include "eth_phy/phy_lan8720.h"
-#include "tcpip_adapter.h"
+#include "modmachine.h"
+#include "machine_uart.h"
 
-#include "modnetwork.h"
+#include "netif/ppp/ppp.h"
+#include "netif/ppp/pppos.h"
+#include "lwip/err.h"
+#include "lwip/sockets.h"
+#include "lwip/sys.h"
+#include "lwip/netdb.h"
+#include "lwip/dns.h"
+#include "lwip/pppapi.h"
+
+extern const mp_obj_type_t machine_uart_type;
 
 typedef struct _ppp_if_obj_t {
     mp_obj_base_t base;
     bool initialized;
     bool active;
     bool connected;
+    ppp_pcb *pcb;
+    machine_uart_obj_t* uart;
+    struct netif ppp_netif;
+    TaskHandle_t client_task_handle;
+
 } ppp_if_obj_t;
 
 const mp_obj_type_t ppp_if_type;
-STATIC lan_if_obj_t ppp_obj = {{&ppp_if_type}, false, false, false};
+STATIC ppp_if_obj_t ppp_obj = {{&ppp_if_type}, false, false, false, NULL, NULL};
 
 
+/* PPP status callback example */
+static void ppp_status_cb(ppp_pcb *pcb, int err_code, void *ctx)
+{
+    ppp_if_obj_t* self = &ppp_obj;
+    struct netif *pppif = ppp_netif(self->pcb);
+    LWIP_UNUSED_ARG(ctx);
 
-STATIC mp_obj_t get_ppp(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args) {
-    lan_if_obj_t* self = &lan_obj;
+    switch (err_code) {
+        case PPPERR_NONE: {
+                              printf("status_cb: Connected\n");
+#if PPP_IPV4_SUPPORT
+                              printf("   our_ipaddr  = %s\n", ipaddr_ntoa(&pppif->ip_addr));
+                              printf("   his_ipaddr  = %s\n", ipaddr_ntoa(&pppif->gw));
+                              printf("   netmask     = %s\n", ipaddr_ntoa(&pppif->netmask));
+#endif /* PPP_IPV4_SUPPORT */
+#if PPP_IPV6_SUPPORT
+                              printf("   our6_ipaddr = %s\n", ip6addr_ntoa(netif_ip6_addr(pppif, 0)));
+#endif /* PPP_IPV6_SUPPORT */
+                              break;
+                          }
+        case PPPERR_PARAM: {
+                               printf("status_cb: Invalid parameter\n");
+                               break;
+                           }
+        case PPPERR_OPEN: {
+                              printf("status_cb: Unable to open PPP session\n");
+                              break;
+                          }
+        case PPPERR_DEVICE: {
+                                printf("status_cb: Invalid I/O device for PPP\n");
+                                break;
+                            }
+        case PPPERR_ALLOC: {
+                               printf("status_cb: Unable to allocate resources\n");
+                               break;
+                           }
+        case PPPERR_USER: {
+                              printf("status_cb: User interrupt\n");
+                              break;
+                          }
+        case PPPERR_CONNECT: {
+                                 printf("status_cb: Connection lost\n");
+                                 break;
+                             }
+        case PPPERR_AUTHFAIL: {
+                                  printf("status_cb: Failed authentication challenge\n");
+                                  break;
+                              }
+        case PPPERR_PROTOCOL: {
+                                  printf("status_cb: Failed to meet protocol\n");
+                                  break;
+                              }
+        case PPPERR_PEERDEAD: {
+                                  printf("status_cb: Connection timeout\n");
+                                  break;
+                              }
+        case PPPERR_IDLETIMEOUT: {
+                                     printf("status_cb: Idle Timeout\n");
+                                     break;
+                                 }
+        case PPPERR_CONNECTTIME: {
+                                     printf("status_cb: Max connect time reached\n");
+                                     break;
+                                 }
+        case PPPERR_LOOPBACK: {
+                                  printf("status_cb: Loopback detected\n");
+                                  break;
+                              }
+        default: {
+                     printf("status_cb: Unknown error code %d\n", err_code);
+                     break;
+                 }
+    }
+
+    /*
+     * This should be in the switch case, this is put outside of the switch
+     * case for example readability.
+     */
+
+    if (err_code == PPPERR_NONE) {
+        return;
+    }
+
+    /* ppp_close() was previously called, don't reconnect */
+    if (err_code == PPPERR_USER) {
+        /* ppp_free(); -- can be called here */
+        return;
+    }
+
+
+    /*
+     * Try to reconnect in 30 seconds, if you need a modem chatscript you have
+     * to do a much better signaling here ;-)
+     */
+    //ppp_connect(pcb, 30);
+    /* OR ppp_listen(pcb); */
+}
+
+STATIC mp_obj_t get_ppp(mp_obj_t uart) {
+    ppp_if_obj_t* self = &ppp_obj;
 
     if (self->initialized) {
-        return MP_OBJ_FROM_PTR(&lan_obj);
+        return MP_OBJ_FROM_PTR(&ppp_obj);
     }
 
-    enum { ARG_uart_id };
-    static const mp_arg_t allowed_args[] = {
-        { MP_QSTR_uart_id, MP_ARG_INT, {.u_int = -1} },
-    };
-
-    mp_arg_val_t args[MP_ARRAY_SIZE(allowed_args)];
-    mp_arg_parse_all(n_args, pos_args, kw_args, MP_ARRAY_SIZE(allowed_args), allowed_args, args);
-
-    if (args[ARG_uart_id].u_obj != mp_const_none) {
-        if (mp_obj_get_int(args[ARG_id].u_int) != 1 && mp_obj_get_int(args[ARG_id].u_int !=2)) {
-            mp_raise_ValueError("invalid LAN interface identifier");
-        }
+    if (! MP_OBJ_IS_TYPE(uart, &machine_uart_type)) {
+        mp_raise_TypeError("requires machine.UART type");
     }
 
+    self->uart = (machine_uart_obj_t*) uart;
     return MP_OBJ_FROM_PTR(&ppp_obj);
 }
-MP_DEFINE_CONST_FUN_OBJ_KW(get_lan_obj, 0, get_lan);
+MP_DEFINE_CONST_FUN_OBJ_1(get_ppp_obj, get_ppp);
 
-STATIC mp_obj_t lan_active(size_t n_args, const mp_obj_t *args) {
-    lan_if_obj_t *self = MP_OBJ_TO_PTR(args[0]);
+static u32_t ppp_output_callback(ppp_pcb *pcb, u8_t *data, u32_t len, void *ctx)
+{
+    ppp_if_obj_t* self = &ppp_obj;
+    mp_stream_p_t* uart_stream = (mp_stream_p_t* )self->uart->base.type->protocol; 
+    int err;
+
+    return uart_stream->write(self->uart, data, len, &err);
+}
+
+static void pppos_client_task() {
+    ppp_if_obj_t* self = &ppp_obj;
+    self->uart->timeout = 100;
+    mp_stream_p_t* uart_stream = (mp_stream_p_t* )self->uart->base.type->protocol; 
+    uint8_t buf[1024];
+    int err;
+    while (ulTaskNotifyTake(pdTRUE, 0) == 0) {
+        int len = uart_stream->read(self->uart, buf, 1024, &err);
+        if (len > 0) {
+            pppos_input_tcpip(self->pcb, (u8_t *)buf, len);
+        }
+    }
+}
+
+
+STATIC mp_obj_t ppp_active(size_t n_args, const mp_obj_t *args) {
+    ppp_if_obj_t *self = MP_OBJ_TO_PTR(args[0]);
 
     if (n_args > 1) {
         if (mp_obj_is_true(args[1])) {
-            /// activate
+            if (self->active) {
+                return mp_const_true;
+            }
+
+            self->pcb = pppapi_pppos_create(&self->ppp_netif, ppp_output_callback, ppp_status_cb, NULL);
+
+            if (self->pcb == NULL) {
+                mp_raise_msg(&mp_type_RuntimeError, "init failed");
+            }
+            pppapi_set_default(self->pcb);
+            //pppapi_set_auth(ppp, PPPAUTHTYPE_PAP, PPP_User, PPP_Pass);
+            pppapi_connect(self->pcb, 0);
+
+            xTaskCreate(pppos_client_task, "ppp", 2048, NULL, 1, &self->client_task_handle);
+            self->active = true;
 
         } else {
-            /// deactivate
+            if (!self->active) {
+                mp_raise_msg(&mp_type_RuntimeError, "already inactive");
+            }
+            xTaskNotifyGive(self->client_task_handle);
+            mp_hal_delay_ms(100);
+            self->active = false;
         }
     }
     return mp_obj_new_bool(self->active);
 }
-STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(lan_active_obj, 1, 2, lan_active);
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(ppp_active_obj, 1, 2, ppp_active);
 
-STATIC mp_obj_t lan_status(mp_obj_t self_in) {
+STATIC mp_obj_t ppp_ifconfig(size_t n_args, const mp_obj_t *args) {
+    ppp_if_obj_t *self = MP_OBJ_TO_PTR(args[0]);
+    struct netif *pppif = ppp_netif(self->pcb);
+    if (n_args == 1) {
+        // get
+        ip_addr_t dns = dns_getserver(0);
+        mp_obj_t tuple[4] = {
+            netutils_format_ipv4_addr((uint8_t*)&pppif->ip_addr, NETUTILS_BIG),
+            netutils_format_ipv4_addr((uint8_t*)&pppif->gw, NETUTILS_BIG),
+            netutils_format_ipv4_addr((uint8_t*)&pppif->netmask, NETUTILS_BIG),
+            netutils_format_ipv4_addr((uint8_t*)&dns, NETUTILS_BIG),
+        };
+        return mp_obj_new_tuple(4, tuple);
+    } else {
+        mp_raise_msg(&mp_type_RuntimeError, "cannot set ifconfig for PPP");
+    }
+}
+MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(ppp_ifconfig_obj, 1, 2, ppp_ifconfig);
+
+STATIC mp_obj_t ppp_status(mp_obj_t self_in) {
     return mp_const_none;
 }
-STATIC MP_DEFINE_CONST_FUN_OBJ_1(lan_status_obj, lan_status);
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(ppp_status_obj, ppp_status);
 
-STATIC mp_obj_t lan_isconnected(mp_obj_t self_in) {
-    lan_if_obj_t *self = MP_OBJ_TO_PTR(self_in);
-    return self->active ? mp_obj_new_bool(self->link_func()) : mp_const_false;
+STATIC mp_obj_t ppp_isconnected(mp_obj_t self_in) {
+    ppp_if_obj_t *self = MP_OBJ_TO_PTR(self_in);
+    return mp_obj_new_bool(self->connected);
 }
-STATIC MP_DEFINE_CONST_FUN_OBJ_1(lan_isconnected_obj, lan_isconnected);
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(ppp_isconnected_obj, ppp_isconnected);
 
-STATIC const mp_rom_map_elem_t lan_if_locals_dict_table[] = {
-    { MP_ROM_QSTR(MP_QSTR_active), MP_ROM_PTR(&lan_active_obj) },
-    { MP_ROM_QSTR(MP_QSTR_isconnected), MP_ROM_PTR(&lan_isconnected_obj) },
-    { MP_ROM_QSTR(MP_QSTR_status), MP_ROM_PTR(&lan_status_obj) },
-    { MP_ROM_QSTR(MP_QSTR_ifconfig), MP_ROM_PTR(&esp_ifconfig_obj) },
+STATIC const mp_rom_map_elem_t ppp_if_locals_dict_table[] = {
+    { MP_ROM_QSTR(MP_QSTR_active), MP_ROM_PTR(&ppp_active_obj) },
+    { MP_ROM_QSTR(MP_QSTR_isconnected), MP_ROM_PTR(&ppp_isconnected_obj) },
+    { MP_ROM_QSTR(MP_QSTR_status), MP_ROM_PTR(&ppp_status_obj) },
+    { MP_ROM_QSTR(MP_QSTR_ifconfig), MP_ROM_PTR(&ppp_ifconfig_obj) },
 };
 
-STATIC MP_DEFINE_CONST_DICT(lan_if_locals_dict, lan_if_locals_dict_table);
+STATIC MP_DEFINE_CONST_DICT(ppp_if_locals_dict, ppp_if_locals_dict_table);
 
-const mp_obj_type_t lan_if_type = {
+const mp_obj_type_t ppp_if_type = {
     { &mp_type_type },
     .name = MP_QSTR_LAN,
-    .locals_dict = (mp_obj_dict_t*)&lan_if_locals_dict,
+    .locals_dict = (mp_obj_dict_t*)&ppp_if_locals_dict,
 };
