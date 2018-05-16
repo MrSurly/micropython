3ab3d25cd2767afb8fbab5b7e74916ec47c55c8c
Eric Poulsen <eric@zyxod.com>
2018-05-16 14:18:13 -0700

    fixed copyright date

--- ports/esp32/machine_uart.h
+++ ports/esp32/machine_uart.h
@@ -4,7 +4,7 @@
  *
  * The MIT License (MIT)
  *
- * Copyright (c) 2017 "Eric Poulsen" <eric@zyxod.com>
+ * Copyright (c) 2018 "Eric Poulsen" <eric@zyxod.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
