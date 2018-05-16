0a3e8f07d522f872bede7b1278bdcd24bf0a364f
Eric Poulsen <eric@zyxod.com>
2018-05-16 14:02:54 -0700

    added semaphore for proper disconnect

--- ports/esp32/network_ppp.c
+++ ports/esp32/network_ppp.c
@@ -34,6 +34,8 @@
 #include "modmachine.h"
 #include "machine_uart.h"
 
+#include "strings.h"
+
 #include "netif/ppp/ppp.h"
 #include "netif/ppp/pppos.h"
 #include "lwip/err.h"
@@ -52,111 +54,37 @@ typedef struct _ppp_if_obj_t {
     bool connected;
     ppp_pcb *pcb;
     machine_uart_obj_t* uart;
-    struct netif ppp_netif;
+    struct netif pppif;
+    SemaphoreHandle_t inactiveWaitSem;
     TaskHandle_t client_task_handle;
-
 } ppp_if_obj_t;
 
 const mp_obj_type_t ppp_if_type;
 STATIC ppp_if_obj_t ppp_obj = {{&ppp_if_type}, false, false, false, NULL, NULL};
 
-
 /* PPP status callback example */
-static void ppp_status_cb(ppp_pcb *pcb, int err_code, void *ctx)
+static void ppp_status_cb(ppp_pcb *pcb, int err_code, void *ctx) // wait for 
 {
     ppp_if_obj_t* self = &ppp_obj;
     struct netif *pppif = ppp_netif(self->pcb);
+    (void)pppif;
     LWIP_UNUSED_ARG(ctx);
 
     switch (err_code) {
         case PPPERR_NONE: {
-                              printf("status_cb: Connected\n");
-#if PPP_IPV4_SUPPORT
-                              printf("   our_ipaddr  = %s\n", ipaddr_ntoa(&pppif->ip_addr));
-                              printf("   his_ipaddr  = %s\n", ipaddr_ntoa(&pppif->gw));
-                              printf("   netmask     = %s\n", ipaddr_ntoa(&pppif->netmask));
-#endif /* PPP_IPV4_SUPPORT */
-#if PPP_IPV6_SUPPORT
-                              printf("   our6_ipaddr = %s\n", ip6addr_ntoa(netif_ip6_addr(pppif, 0)));
-#endif /* PPP_IPV6_SUPPORT */
+                              self->connected = (pppif->ip_addr.u_addr.ip4.addr != 0);
                               break;
                           }
-        case PPPERR_PARAM: {
-                               printf("status_cb: Invalid parameter\n");
-                               break;
-                           }
-        case PPPERR_OPEN: {
-                              printf("status_cb: Unable to open PPP session\n");
-                              break;
-                          }
-        case PPPERR_DEVICE: {
-                                printf("status_cb: Invalid I/O device for PPP\n");
-                                break;
-                            }
-        case PPPERR_ALLOC: {
-                               printf("status_cb: Unable to allocate resources\n");
-                               break;
-                           }
         case PPPERR_USER: {
-                              printf("status_cb: User interrupt\n");
+                              xSemaphoreGive(self->inactiveWaitSem);
                               break;
                           }
         case PPPERR_CONNECT: {
-                                 printf("status_cb: Connection lost\n");
+                                 self->connected = false;
                                  break;
                              }
-        case PPPERR_AUTHFAIL: {
-                                  printf("status_cb: Failed authentication challenge\n");
-                                  break;
-                              }
-        case PPPERR_PROTOCOL: {
-                                  printf("status_cb: Failed to meet protocol\n");
-                                  break;
-                              }
-        case PPPERR_PEERDEAD: {
-                                  printf("status_cb: Connection timeout\n");
-                                  break;
-                              }
-        case PPPERR_IDLETIMEOUT: {
-                                     printf("status_cb: Idle Timeout\n");
-                                     break;
-                                 }
-        case PPPERR_CONNECTTIME: {
-                                     printf("status_cb: Max connect time reached\n");
-                                     break;
-                                 }
-        case PPPERR_LOOPBACK: {
-                                  printf("status_cb: Loopback detected\n");
-                                  break;
-                              }
-        default: {
-                     printf("status_cb: Unknown error code %d\n", err_code);
-                     break;
-                 }
+        default: break;
     }
-
-    /*
-     * This should be in the switch case, this is put outside of the switch
-     * case for example readability.
-     */
-
-    if (err_code == PPPERR_NONE) {
-        return;
-    }
-
-    /* ppp_close() was previously called, don't reconnect */
-    if (err_code == PPPERR_USER) {
-        /* ppp_free(); -- can be called here */
-        return;
-    }
-
-
-    /*
-     * Try to reconnect in 30 seconds, if you need a modem chatscript you have
-     * to do a much better signaling here ;-)
-     */
-    //ppp_connect(pcb, 30);
-    /* OR ppp_listen(pcb); */
 }
 
 STATIC mp_obj_t get_ppp(mp_obj_t uart) {
@@ -171,6 +99,9 @@ STATIC mp_obj_t get_ppp(mp_obj_t uart) {
     }
 
     self->uart = (machine_uart_obj_t*) uart;
+    self->inactiveWaitSem = xSemaphoreCreateBinary();
+    assert(self->inactiveWaitSem != NULL);
+    self->initialized = true;
     return MP_OBJ_FROM_PTR(&ppp_obj);
 }
 MP_DEFINE_CONST_FUN_OBJ_1(get_ppp_obj, get_ppp);
@@ -186,18 +117,19 @@ static u32_t ppp_output_callback(ppp_pcb *pcb, u8_t *data, u32_t len, void *ctx)
 
 static void pppos_client_task() {
     ppp_if_obj_t* self = &ppp_obj;
-    self->uart->timeout = 100;
+    self->uart->timeout = 50;
     mp_stream_p_t* uart_stream = (mp_stream_p_t* )self->uart->base.type->protocol; 
-    uint8_t buf[1024];
+    uint8_t buf[256];
+
     int err;
     while (ulTaskNotifyTake(pdTRUE, 0) == 0) {
-        int len = uart_stream->read(self->uart, buf, 1024, &err);
+        int len = uart_stream->read(self->uart, buf, sizeof(buf), &err);
         if (len > 0) {
             pppos_input_tcpip(self->pcb, (u8_t *)buf, len);
         }
     }
+    vTaskDelete(NULL);
 }
-
 
 STATIC mp_obj_t ppp_active(size_t n_args, const mp_obj_t *args) {
     ppp_if_obj_t *self = MP_OBJ_TO_PTR(args[0]);
@@ -208,46 +140,67 @@ STATIC mp_obj_t ppp_active(size_t n_args, const mp_obj_t *args) {
                 return mp_const_true;
             }
 
-            self->pcb = pppapi_pppos_create(&self->ppp_netif, ppp_output_callback, ppp_status_cb, NULL);
+            self->pcb = pppapi_pppos_create(&self->pppif, ppp_output_callback, ppp_status_cb, NULL);
 
             if (self->pcb == NULL) {
                 mp_raise_msg(&mp_type_RuntimeError, "init failed");
             }
             pppapi_set_default(self->pcb);
-            //pppapi_set_auth(ppp, PPPAUTHTYPE_PAP, PPP_User, PPP_Pass);
             pppapi_connect(self->pcb, 0);
 
-            xTaskCreate(pppos_client_task, "ppp", 2048, NULL, 1, &self->client_task_handle);
+            xTaskCreate(pppos_client_task, "ppp", 512, NULL, 1, &self->client_task_handle);
             self->active = true;
 
         } else {
             if (!self->active) {
-                mp_raise_msg(&mp_type_RuntimeError, "already inactive");
+                return mp_const_false;
             }
+
+            // Wait for PPPERR_USER
+            pppapi_close(self->pcb, 0);
+            xSemaphoreTake(self->inactiveWaitSem, portMAX_DELAY); 
+            xSemaphoreGive(self->inactiveWaitSem);
+
+            // Shutdown task
             xTaskNotifyGive(self->client_task_handle);
-            mp_hal_delay_ms(100);
+            while(eTaskGetState(self->client_task_handle) != eDeleted) {
+                mp_hal_delay_ms(10);
+            }
+
+            pppapi_free(self->pcb);
+            self->pcb = NULL;
             self->active = false;
+            self->connected = false;
         }
     }
     return mp_obj_new_bool(self->active);
 }
-STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(ppp_active_obj, 1, 2, ppp_active);
-
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(ppp_active_obj, 1, 2, ppp_active); 
 STATIC mp_obj_t ppp_ifconfig(size_t n_args, const mp_obj_t *args) {
     ppp_if_obj_t *self = MP_OBJ_TO_PTR(args[0]);
-    struct netif *pppif = ppp_netif(self->pcb);
+    ip_addr_t dns;
     if (n_args == 1) {
         // get
-        ip_addr_t dns = dns_getserver(0);
-        mp_obj_t tuple[4] = {
-            netutils_format_ipv4_addr((uint8_t*)&pppif->ip_addr, NETUTILS_BIG),
-            netutils_format_ipv4_addr((uint8_t*)&pppif->gw, NETUTILS_BIG),
-            netutils_format_ipv4_addr((uint8_t*)&pppif->netmask, NETUTILS_BIG),
-            netutils_format_ipv4_addr((uint8_t*)&dns, NETUTILS_BIG),
-        };
-        return mp_obj_new_tuple(4, tuple);
+        if (self->pcb != NULL) {
+            dns = dns_getserver(0);
+            struct netif *pppif = ppp_netif(self->pcb);
+            mp_obj_t tuple[4] = {
+                netutils_format_ipv4_addr((uint8_t*)&pppif->ip_addr, NETUTILS_BIG),
+                netutils_format_ipv4_addr((uint8_t*)&pppif->gw, NETUTILS_BIG),
+                netutils_format_ipv4_addr((uint8_t*)&pppif->netmask, NETUTILS_BIG),
+                netutils_format_ipv4_addr((uint8_t*)&dns, NETUTILS_BIG),
+            };
+            return mp_obj_new_tuple(4, tuple);
+        } else {
+            mp_obj_t tuple[4] = { mp_const_none, mp_const_none, mp_const_none, mp_const_none };
+            return mp_obj_new_tuple(4, tuple);
+        }
     } else {
-        mp_raise_msg(&mp_type_RuntimeError, "cannot set ifconfig for PPP");
+        mp_obj_t *items;
+        mp_obj_get_array_fixed_n(args[1], 4, &items);
+        netutils_parse_ipv4_addr(items[3], (uint8_t*)&dns.u_addr.ip4, NETUTILS_BIG);
+        dns_setserver(0, & dns);
+        return mp_const_none;
     }
 }
 MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(ppp_ifconfig_obj, 1, 2, ppp_ifconfig);
@@ -274,6 +227,6 @@ STATIC MP_DEFINE_CONST_DICT(ppp_if_locals_dict, ppp_if_locals_dict_table);
 
 const mp_obj_type_t ppp_if_type = {
     { &mp_type_type },
-    .name = MP_QSTR_LAN,
+    .name = MP_QSTR_PPP,
     .locals_dict = (mp_obj_dict_t*)&ppp_if_locals_dict,
 };
